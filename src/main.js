c5da698eddc9be24e86116b0a14b8e0143476278
Patrick-Nguedi <patricknguedi08@gmail.com>
2022-08-02 19:12:52 +0100

    End of ajustment i18n

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 import { createApp } from 'vue'
 import './tailwind.css'
-import I18n from "./i18n.json";
+import { createI18n } from "vue-i18n";
+import i18nMessages from "./i18n.json";
 import App from './App.vue'
 
-createApp(I18n).mount('#i18n')
-createApp(App).mount('#app')
+const defaultLanguage = () => {
+  const result = navigator.language.split("-")[0];
+  if (result !== "fr") {
+    return "en";
+  }
+  return result;
+};
+
+const locale = defaultLanguage();
+const i18n = createI18n({
+  locale,
+  legacy: false,
+  fallbackLocale: "en",
+  messages: i18nMessages,
+});
+
+
+
+createApp(App)
+    .use(i18n)
+    .mount("#app");
